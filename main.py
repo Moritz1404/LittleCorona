0067657e9006f6241e28325d792f0cb49463ada2
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-06-04 09:41:16 +0200

    main.py: added formatDate(), added 'Update date', rename test.sh => start.sh

--- main.py
+++ main.py
@@ -10,29 +10,44 @@ def cohs(param):
     # Get Deutschland
     deutschland = requests.get('https://api.corona-zahlen.org/germany')
     deutschland_data = deutschland.json()
+    deutschland_date = formatDate(deutschland_data['meta']['lastCheckedForUpdate'])
 
     # Get NRW
     nrw = requests.get('https://api.corona-zahlen.org/states/NW')
     nrw_data = nrw.json()
+    nrw_date = formatDate(nrw_data['meta']['lastCheckedForUpdate'])
 
     # Get Heinsberg
     heinsberg = requests.get('https://api.corona-zahlen.org/districts/05370')
     heinsberg_data = heinsberg.json()
+    heinsberg_date = formatDate(heinsberg_data['meta']['lastCheckedForUpdate'])
 
     if param == 'd' or param == '0':
         # Print Deutschland
         print('Deutschland:')
-        print(str(deutschland_data['weekIncidence']) [:LIM_DIGITS] + '\n')
+        print(str(deutschland_data['weekIncidence']) [:LIM_DIGITS])
+        print('Updated: ' + deutschland_date + '\n')
 
     if param == 'n' or param == '0':
         # Print NRW
         print(nrw_data['data']['NW']['name'] + ':')
-        print(str(nrw_data['data']['NW']['weekIncidence']) [:LIM_DIGITS] + '\n') 
+        print(str(nrw_data['data']['NW']['weekIncidence']) [:LIM_DIGITS]) 
+        print('Updated: ' + nrw_date + '\n')
 
     if param == 'h' or param == '0':
         # Print Heinsberg
         print(heinsberg_data['data'][ags]['name'] + ':')
-        print(str(heinsberg_data['data'][ags]['weekIncidence']) [:LIM_DIGITS] + '\n') 
+        print(str(heinsberg_data['data'][ags]['weekIncidence']) [:LIM_DIGITS])
+        print('Updated: ' + heinsberg_date + '\n') 
+
+
+def formatDate(date):
+    year = date[0:10]
+    time = date[12:16]
+    
+    newDate = year + ' ' + time
+
+    return newDate
 
 
 if __name__=="__main__":
